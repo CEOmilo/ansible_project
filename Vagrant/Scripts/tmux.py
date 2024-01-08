a4102e1627a280e397ff364a2e42d88a2489ba56
CEOmilo <smwilliamsii@yahoo.com>
2024-01-08 15:00:46 -0800

    Added copying of website to vm

--- Vagrant/Scripts/tmux.py
+++ Vagrant/Scripts/tmux.py
@@ -1,4 +1,11 @@
 import paramiko
+import logging
+import os
+
+# Setting up logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__) 
 
 # Establishing ssh connection
 
@@ -10,43 +17,63 @@ def establish_ssh_connection(hostname, port, username, private_key_path):
     try:
         # Connect to the remote host using the provided details
         ssh.connect(hostname, port=port, username=username, key_filename=private_key_path)
-        print(f"SSH connection established to {hostname}")
+        logger.info("SSH connection established to {hostname}")
         return ssh
     except Exception as e:
-        print(f"Error connecting to {hostname}: {e}")
+        logger.info(f"Error connecting to {hostname}: {e}")
         return None
     
+# Checking if a web server is already installed
 def is_apache_installed(ssh):
     try:
         # Check if apache2 is installed
         _, stdout, _ = ssh.exec_command("which apache2")
         return bool(stdout.read().decode('utf-8').strip())
     except Exception as e:
-        print(f"Error checking apache2 installation: {e}")
+        logger.info("Error checking apache2 installation: {e}")
         return False
     
-# Deploying apache web servers to vm's
+# Deploying apache web servers to designated vm
 
 def deploy_apache(ssh):
     try:
         # Checking if apache is installed
         if is_apache_installed(ssh):
-            print("apache is already installed on the system.")
+            logger.info("apache is already installed on the system.")
         else:
-            # Run tmux deployment commands
+            # Run apache deployment commands
             apache_install_command = "sudo apt-get update && sudo apt-get install -y apache2"
             _, stdout, stderr = ssh.exec_command(apache_install_command)
 
             # Check if the installation was successful
             if stdout.channel.recv_exit_status() == 0:
-                print("Successfully installed apache")
+                logger.info("Successfully installed apache")
             else:
-                print(f"Failed to deploy apache. Error: {stderr.read().decode('utf-8')}")
+                logger.info("Failed to deploy apache. Error: {stderr.read().decode('utf-8')}")
     except Exception as e:
-        print(f"Error during apache deployment: {e}")
+        logger.info(f"Error during apache deployment: {e}")
         return False
 
-        
+# Copying my local website to the vm
+
+def deploy_website(ssh, website_path):
+    ssh.exec_command('sudo chmod -R 777 /var/www/html')
+    sftp = ssh.open_sftp()
+    for root, dirs, files in os.walk(website_path):
+            for file in files:
+                local_file_path = os.path.join(root, file)
+                remote_file_path = os.path.join(website_path, os.path.relpath(local_file_path, website_path))
+
+                # Ensure remote directories exist
+                remote_dir = os.path.dirname(remote_file_path)
+                try:
+                    sftp.stat(remote_dir)
+                except FileNotFoundError:
+                    sftp.mkdir(remote_dir)
+
+                # Upload the file
+                sftp.put(local_file_path, remote_file_path)
+    sftp.close()
 
 
 # Checking if TMUX is installed
@@ -57,7 +84,7 @@ def is_tmux_installed(ssh):
         _, stdout, _ = ssh.exec_command("which tmux")
         return bool(stdout.read().decode('utf-8').strip())
     except Exception as e:
-        print(f"Error checking tmux installation: {e}")
+        logger.info("Error checking tmux installation: {e}")
         return False
 
 # Deploying TMUX to all of our VM's
@@ -66,7 +93,7 @@ def deploy_tmux(ssh):
     try:
         # Check if tmux is already installed
         if is_tmux_installed(ssh):
-            print("tmux is already installed on the system.")
+            logger.info("tmux is already installed on the system.")
         else:
             # Run tmux deployment commands
             tmux_install_command = "sudo apt-get update && sudo apt-get install -y tmux"
@@ -74,11 +101,11 @@ def deploy_tmux(ssh):
 
             # Check if the installation was successful
             if stdout.channel.recv_exit_status() == 0:
-                print("Successfully deployed tmux")
+                logger.info("Successfully deployed tmux")
             else:
-                print(f"Failed to deploy tmux. Error: {stderr.read().decode('utf-8')}")
+                logger.info("Failed to deploy tmux. Error: {stderr.read().decode('utf-8')}")
     except Exception as e:
-        print(f"Error during tmux deployment: {e}")
+        logger.info("Error during tmux deployment: {e}")
         return False
 
 # Creating sample file
@@ -91,11 +118,11 @@ def create_example_file(ssh):
 
         # Check if the file creation was successful
         if stdout.channel.recv_exit_status() == 0:
-            print("Successfully created example.txt")
+            logger.info("Successfully created example.txt")
         else:
-            print(f"Failed to create example.txt. Error: {stderr.read().decode('utf-8')}")
+            logger.info("Failed to create example.txt. Error: {stderr.read().decode('utf-8')}")
     except Exception as e:
-        print(f"Error creating example.txt: {e}")
+        logger.info("Error creating example.txt: {e}")
         return False
 
 # This is where we read our parsed inventory file and return it
@@ -141,13 +168,32 @@ def main():
     inventory = parse_inventory_file(vagrant_inventory_path)
     # Define my web server
     web_server_vm = "vm1"
+    # Defining local and remote path for website files, which is the same path
+    website_path = "/var/www/html"
+    # Storing information in a list of dictionaries
+    connection_details_list = []
 
     for group, hosts in inventory.items():
         for host in hosts:
             connection_details = extract_connection_details(host, vagrant_inventory_path)
             if connection_details:
-                hostname, port, username, private_key_path, vm_identifier = connection_details
-                print(f"VM: {vm_identifier}, Host: {hostname}, Port: {port}, Username: {username}, Private Key Path: {private_key_path}")
+                connection_details_list.append({
+                    'hostname': connection_details[0],
+                    'port': connection_details[1],
+                    'username': connection_details[2],
+                    'private_key_path': connection_details[3],
+                    'vm_identifier': connection_details[4]
+                })
+                for details in connection_details_list:
+                    hostname = details['hostname']
+                    port = details['port']
+                    username = details['username']
+                    private_key_path = details['private_key_path']
+                    vm_identifier = details['vm_identifier']
+            
+            
+                logger.info(f"VM: {vm_identifier}, Host: {hostname}, Port: {port}, Username: {username}, Private Key Path: {private_key_path}")
+                
                 # Establish SSH connection
                 ssh_connection = establish_ssh_connection(hostname, port, username, private_key_path)
 
@@ -159,12 +205,14 @@ def main():
                     if vm_identifier == web_server_vm :
                         # Deploy apache on the remote host
                         deploy_apache(ssh_connection)
+                        # Use paramiko to scp the website to the web server
+                        deploy_website(ssh_connection, website_path)
                     else:
-                        print(f"Skipping web server")
+                        logger.info("Skipping web server")
                     # Close the ssh connection
                     ssh_connection.close()
             else:
-                print(f"Skipping invalid host line: {host}")
+                logger.info("Skipping invalid host line: {host}")
 
 if __name__ == "__main__":
     main()
